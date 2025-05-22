87a1fb126be513da9a24bc67dcf2f0c6ca072d34
Diah082 <152632332+Diah082@users.noreply.github.com>
2025-05-22 16:17:26 +0700

    Update install-handler.sh

--- install-handler.sh
+++ install-handler.sh
@@ -6,4 +6,6 @@ if [[ ! -f /root/Install.sh ]]; then
     wget -q https://raw.githubusercontent.com/Diah082/vip/main/Install.sh -O /root/Install.sh
     chmod +x /root/Install.sh
 fi
-nohup /root/Install.sh "$NAMA" "$DOMAIN" > "$LOGFILE" 2>&1 &
+screen -dmS install_${NAMA} bash -c "/root/Install.sh ${NAMA} ${DOMAIN} | tee ${LOG_FILE}"
+echo "✅ Proses instalasi untuk $NAMA dimulai di screen: install_${NAMA}"
+echo "ℹ️ Lihat log: screen -r install_${NAMA}  atau cek /root/log_install_${NAMA}.log"
