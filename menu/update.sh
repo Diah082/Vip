4ff34b703af8481e06003193111d2649a886007e
Diah082 <152632332+Diah082@users.noreply.github.com>
2026-04-02 11:58:05 +0700

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd
 rm -- "$0"
-wget -qO updatesc.sh "http://getbot.nevpn.my.id:3050/update.sh"
+wget -qO updatesc.sh "http://getbot.nevpn.my.id:3051/update.sh"
 chmod +x updatesc.sh
 ./updatesc.sh
